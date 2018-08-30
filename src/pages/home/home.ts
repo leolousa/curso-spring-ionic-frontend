a204dbed4a7c0c56d5138168b9621cbbb2ae87d2
leolousa <leolousa@hotmail.com>
2018-08-30 11:30:41 -0300

    Lazy loading das páginas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 
+@IonicPage()// Decorator da página
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  // Controlador da View
   constructor(public navCtrl: NavController) {
 
   }
