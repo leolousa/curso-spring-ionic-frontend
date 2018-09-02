ca5438d74083f2b575c919175b930ef4a0bd32d1
leolousa <leolousa@hotmail.com>
2018-09-02 11:57:56 -0300

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Classe de erro global
@@ -9,7 +10,10 @@ import { StorageService } from '../services/storage.service';
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(public storage: StorageService) {}
+  constructor(
+    public storage: StorageService,
+    public alertCtrl: AlertController
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log("Passou no Interceptor!");
@@ -28,8 +32,21 @@ export class ErrorInterceptor implements HttpInterceptor {
           console.log(errorObj);
 
           switch(errorObj.status) {
-            case 403:
+
+            case 401: // Erro de autenticação
+            this.handle401();
+            break;
+
+            case 403: // Acesso negado - Autenticação falhou
             this.handle403();
+            break;
+
+            case 404: // Recurso não encontrado
+            this.handle404();
+            break;
+
+            default:
+            this.handleDefaultError(errorObj);
             break;
           }
           // propaga o erro para o controlador
@@ -41,6 +58,45 @@ export class ErrorInterceptor implements HttpInterceptor {
     this.storage.setLocalUser(null);
   }
 
+  handle404() {
+    let alert = this.alertCtrl.create({
+      title: 'Erro 404: Recurso não disponível!',
+      message: 'Não foi possível acessar o recurso neste momento',
+      enableBackdropDismiss: false,
+      buttons: [
+        { text: 'Ok' }
+      ]
+    });
+
+    alert.present();
+  }
+
+  handle401() {
+
+    let alert = this.alertCtrl.create({
+      title: 'Erro 401: Falha de autenticação!',
+      message: 'Email ou senha incorretos',
+      enableBackdropDismiss: false,
+      buttons: [
+        { text: 'Ok' }
+      ]
+    });
+
+    alert.present();
+  }
+
+  handleDefaultError(errorObj) {
+    let alert = this.alertCtrl.create({
+      title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [
+        { text: 'Ok' }
+      ]
+    });
+
+    alert.present();
+  }
 }
 
 // Exigências para criação do Interceptor
