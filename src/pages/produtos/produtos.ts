62bf17b370df32841742d9534057880c610d151f
leolousa <leolousa@hotmail.com>
2018-09-08 01:30:22 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -22,6 +22,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.carregaDados();
+  }
+
+  carregaDados() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
 
@@ -59,5 +63,12 @@ export class ProdutosPage {
     });
     loader.present();
     return loader;
+  }
+
+  doRefresh(refresher) {
+    this.carregaDados();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
   }
 }
